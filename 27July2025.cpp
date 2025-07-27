7989c5f8ad0504f250a11a2784d186e54caf5c69
Shivam Mehta <116088341+ShivamMehta161@users.noreply.github.com>
2025-07-27 23:13:37 +0530

    Update 27July2025.cpp

--- 27July2025.cpp
+++ 27July2025.cpp
@@ -1,27 +1,34 @@
-public class Solution {
-    public int CountHillValley(int[] nums) {
-        if(nums.Length<3) return 0;
-        int l=0;
-        int i=1;
-        int c=0;
-        for(int r =2; r<nums.Length;r++){
-            if(nums[l]==nums[i]){
-                i++;
-                l++;
-            }
-            else if(nums[r]==nums[i]){
-                continue;
-            }
-            else{
-                if((nums[l]<nums[i] && nums[i]>nums[r]) || (nums[l]>nums[i] && nums[i]<nums[r]) ){
-                    c++;
-                }
-                
-                    l=r-1;
-                    i=r;
+class Solution 
+{
+public:
+    int countHillValley(vector<int>& nums) 
+    {
+        
+        vector<int> filtered;
+        filtered.push_back(nums[0]);
+        for (int i = 1; i < nums.size(); ++i) 
+        {
+            if (nums[i] != nums[i - 1]) 
+            {
+                filtered.push_back(nums[i]);
             }
         }
-        return c;
 
+
+        int count = 0;
+        for (int i = 1; i < filtered.size() - 1; ++i) 
+        {
+            int prev = filtered[i - 1];
+            int curr = filtered[i];
+            int next = filtered[i + 1];
+
+            
+            if ((curr > prev && curr > next) || (curr < prev && curr < next)) 
+            {
+                ++count;  
+            }
+        }
+
+        return count;
     }
-}
+};
